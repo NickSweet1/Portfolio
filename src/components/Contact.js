32f999272b26f9128cfc32b13b04005f9d51fb40
Nick <nicksweetj@gmail.com>
2024-01-20 01:42:22 -0500

    restyled the portfolio and the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,88 +1,116 @@
-import {React, useRef } from "react";
-import emailjs from '@emailjs/browser';
+import { React, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const form = useRef();
+  const [isFormFilling, setFormFilling] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_jdks8et', 'template_07yovwk', form.current, 'uS4JKS5KrzcNLqctZ')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_jdks8et",
+        "template_07yovwk",
+        form.current,
+        "uS4JKS5KrzcNLqctZ"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
-    };
+        }
+      );
+    e.target.reset();
+    setFormFilling(false);
+  };
+
+  const handleFormFocus = () => {
+    setFormFilling(true);
+    console.log(isFormFilling);
+  };
+
+  const handleFormBlur = () => {
+    setFormFilling(false);
+  };
 
   return (
-    <section className="background py-16">
+    <section className='background py-16'>
       <div className="container mx-auto flex flex-col md:flex-row">
         <div className="md:w-1/2 flex justify-center">
           <img
-            src={process.env.PUBLIC_URL + '/images/IMG-4884.JPG'} // Replace with image path
+            src={process.env.PUBLIC_URL + "/images/IMG-4884.JPG"} // Replace with image path
             alt="Contact Image"
             className="w-5/6 rounded-full "
           />
         </div>
 
         <div className="pt-5 sm:pt-0 md:w-1/2  flex flex-col justify-center items-center">
-          <h2 className="text-3xl text-white font-semibold mb-6 flex justify-center">Contact Us</h2>
-          <form ref={form} onSubmit={sendEmail}>
-            <div className="mb-4 flex justify-center">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
-                placeholder="Your Name"
-                required
-              />
-            </div>
+        <div className={`bg-tan border border-white rounded-2xl ${isFormFilling ? 'bg-opacity-100' : 'bg-opacity-80'}`}>
+            <h2 className="text-3xl text-white font-semibold mb-6 flex justify-center pt-6">
+              Contact Us
+            </h2>
+            <form ref={form} onSubmit={sendEmail} onBlur={handleFormBlur}>
+              <div className="mb-4 flex justify-center">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="ml-6 mr-6 w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
+                  placeholder="Your Name"
+                  onFocus={handleFormFocus}
+                  required
+                />
+              </div>
 
-            <div className="mb-4 flex justify-center">
-              <input
-                type="text"
-                id="subject"
-                name="subject"
-                className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
-                placeholder="Subject"
-                required
-              />
-            </div>
+              <div className="mb-4 flex justify-center">
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
+                  placeholder="Subject"
+                  onFocus={handleFormFocus}
+                  required
+                />
+              </div>
 
-            <div className="mb-4 flex justify-center">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
-                placeholder="Your Email"
-                required
-              />
-            </div>
+              <div className="mb-4 flex justify-center">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
+                  placeholder="Your Email"
+                  onFocus={handleFormFocus}
+                  required
+                />
+              </div>
 
-            <div className="mb-4 flex justify-center">
-              <textarea
-                id="message"
-                name="message"
-                rows="4"
-                className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
-                placeholder="Your Message"
-                required
-              ></textarea>
-            </div>
+              <div className="mb-4 flex justify-center">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="4"
+                  className="w-11/12 sm:w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-tan"
+                  placeholder="Your Message"
+                  onFocus={handleFormFocus}
+                  required
+                ></textarea>
+              </div>
 
-            <div className="mt-6 flex justify-center">
-              <button
-                type="submit"
-                className="px-6 py-3 bg-beige text-tan font-semibold rounded-lg hover:bg-tan hover:text-beige focus:outline-none focus:bg-beige focus:text-tan"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+              <div className="mt-6 flex justify-center pb-6">
+                <button
+                  type="submit"
+                  className="px-6 py-3 bg-beige text-tan font-semibold rounded-lg hover:bg-olive hover:shadow-white hover:text-beige focus:outline-none focus:bg-beige focus:text-tan"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
-import { projects } from "../constants/index"
+import { projects } from "../constants/index";
 
 function Portfolio() {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -11,172 +11,79 @@ function Portfolio() {
     setSelectedProject(projects[index]);
   };
 
-  const isMobile = window.innerWidth < 640;
-
   return (
     <section id="portfolio" className="background py-16">
-      <div className="container mx-auto  flex justify-center">
-        {isMobile ? (
-          <div className="text-center">
-            <h2 className="text-4xl text-beige font-semibold mb-6 flex justify-center">
-              Projects
-            </h2>
-            <p className="m-5 border border-beige rounded-lg text-beige pt-1 pr-3 pl-4 pb-3 text-center bg-tan">
-              Welcome to my portfolio, where you can check out some cool
-              projects I tackled during my Full Stack boot camp. These projects
-              reflect different aspects of the course, and if you're curious
-              for more details, just click on the project titles below!
-            </p>
-            <ul>
-              {projects.map((project, index) => (
-                <li key={index} className="mb-4">
-                  <h3
-                    className={`text-xl text-beige font-semibold cursor-pointer m-5 ${
-                      selectedProject === project
-                        ? "border border-beige rounded p-2 bg-tan hover:shadow-lg hover:translate-x-0.5 hover:-translate-y-0.5"
-                        : ""
-                    }`}
-                    onClick={() => handleProjectClick(index)}
-                  >
-                    {project.title}
-                  </h3>
-                  {selectedProject === project && (
-                    <div className="border border-beige rounded-lg pt-1 pr-3 pl-3 pb-3 bg-tan m-5">
-                      <p className="text-beige">{project.description}</p>
-                      <h3 className="text-beige text-lg underline flex justify-center mt-6">
-                        <a
-                          className="pr-6"
-                          href={project.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FontAwesomeIcon
-                            icon={faSquareGithub}
-                            className="fa-2x text-beige"
-                          />
-                        </a>
-                        {project.video && (
-                          <a
-                            className="pl-6"
-                            href={project.video}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <FontAwesomeIcon
-                              icon={faYoutube}
-                              className="fa-2x text-beige"
-                            />
-                          </a>
-                        )}
-                        {project.sitelink && (
-                          <a
-                            className="pl-6"
-                            href={project.sitelink}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <FontAwesomeIcon
-                              icon={faGlobe}
-                              className="fa-2x text-beige"
-                            />
-                          </a>
-                        )}
-                      </h3>
-                    </div>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <div className="flex flex-col md:flex-row max-w-screen-lg">
-            <div className="md:w-1/2 pr-12">
-              {selectedProject ? (
-                <div>
-                  <h2 className="text-4xl text-beige font-semibold flex justify-center pb-6">
-                    {selectedProject.title}
-                  </h2>
-                  <div className="border border-beige rounded-lg pt-1 pr-3 pl-3 pb-3 bg-tan">
-                    <p className="text-beige">{selectedProject.description}</p>
-                    <h3 className="text-beige text-lg underline flex justify-center mt-6">
-                      <a
-                        className="pr-6"
-                        href={selectedProject.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FontAwesomeIcon
-                          icon={faSquareGithub}
-                          className="fa-2x text-beige"
-                        />
-                      </a>
-                      {selectedProject.video && (
-                        <a
-                          className="pl-6"
-                          href={selectedProject.video}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FontAwesomeIcon
-                            icon={faYoutube}
-                            className="fa-2x text-beige"
-                          />
-                        </a>
-                      )}
-                      {selectedProject.sitelink && (
-                        <a
-                          className="pl-6"
-                          href={selectedProject.sitelink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FontAwesomeIcon
-                            icon={faGlobe}
-                            className="fa-2x text-beige"
-                          />
-                        </a>
-                      )}
-                    </h3>
-                  </div>
-                </div>
-              ) : (
-                <div>
-                  <h2 className="text-4xl text-beige font-semibold mb-6 flex justify-center">
-                    Projects
-                  </h2>
-                  <p className="border border-beige rounded-lg text-beige pt-1 pr-3 pl-4 pb-3 text-center bg-tan">
-                    Welcome to my portfolio, where you can check out some cool
-                    projects I tackled during my Full Stack boot camp. These
-                    projects reflect different aspects of the course, and if
-                    you're curious for more details, just click on the project
-                    titles to the right!
-                  </p>
-                </div>
-              )}
-            </div>
+      <div className="pl-5 pr-5 w-full mx-auto text-2xl md:w-1/2 md:pl-0 md:pr-0">
+        <div>
+          <p className="pb-5 pt-3 md:pb-3 border-olive border-4 rounded-lg text-beige pr-3 pl-4 text-center bg-tan bg-opacity-90">
+            Welcome to my portfolio, where you can check out some cool projects
+            I tackled during my Full Stack boot camp. These projects reflect
+            different aspects of the course, and if you're curious for more
+            details, just click on the project titles to the right!
+          </p>
+        </div>
+      </div>
 
-            <div className="md:w-1/2 mt-6 md:mt-0 pl-12">
-              <h1 className="text-4xl text-beige font-semibold mb-6 flex justify-center">
-                Recent Works
-              </h1>
-              <ul className="text-center">
-                {projects.map((project, index) => (
-                  <li key={index} className="mb-4">
-                    <h3
-                      className="text-xl text-beige font-semibold cursor-pointer p-2"
+      <div className="grid cols-3 container mx-auto justify-center">
+        <h1 className="text-4xl text-beige font-semibold mb-6 flex justify-center pt-10">
+          Recent Works
+        </h1>
+        <div className="mx-auto w-9/12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className="border-2 border-olive border-solid p-4 bg-tan bg-opacity-90 rounded-xl transition duration-300 ease-in-out hover:bg-opacity-100 hover:shadow-2xl"
+            >
+              <div className="text-center text-white text-2xl pb-6">
+                {project.title}
+              </div>
+              <div className="text-beige text-center">
+                {project.description}
+              </div>
+              <div className="mx-auto grid grid-cols-2 w-1/2 justify-center mt-4">
+                {project.video && (
+                  <div className="flex mt-4">
+                    <a
+                      href={project.video}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-beige flex items-center mx-auto"
                     >
-                      <span className="border border-beige rounded p-2 bg-tan hover:shadow-lg hover:translate-x-0.5 hover:-translate-y-0.5"
-                        onClick={() => handleProjectClick(index)}
-                      >
-                        {project.title}
-                      </span>
-                    </h3>
-                  </li>
-                ))}
-              </ul>
+                      <FontAwesomeIcon icon={faYoutube} className="fa-2x" />
+                    </a>
+                  </div>
+                )}
+                {project.link && (
+                  <div className="flex mt-4">
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-beige flex items-center mx-auto"
+                    >
+                      <FontAwesomeIcon
+                        icon={faSquareGithub}
+                        className="fa-2x"
+                      />
+                    </a>
+                  </div>
+                )}
+                {project.sitelink && (
+                  <div className="flex mt-4">
+                    <a
+                      href={project.sitelink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-beige flex items-center mx-auto"
+                    >
+                      <FontAwesomeIcon icon={faGlobe} className="fa-2x" />
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        )}
+          ))}
+        </div>
       </div>
     </section>
   );
