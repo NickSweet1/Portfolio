2ab10e19d98ea9cfb8b611191f97905bdcbb8292
Nick <nicksweetj@gmail.com>
2024-01-19 23:44:23 -0500

    moved constants to seperate file

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+export const projects = [
+    {
+      title: "Stasio's Italian Deli",
+      link: "https://github.com/NickSweet1/Stasios",
+      sitelink:
+        "https://stasios-7fcd485311cd.herokuapp.com/",
+      description:
+        "This website has a sleek design and covers everything about the menu, hours, and history. It's powered by a GraphQL database for that extra menu flair, and the manager can log in to make menu tweaks using CRUD (Create, Read, Update, Delete) operations.",
+    },
+    {
+      title: "Mezcal Employee Portal",
+      sitelink: "https://mezcal-book-580ae53d198b.herokuapp.com",
+      link: "https://github.com/JoshuaVaneps/Mezcal-Book",
+      description:
+        "Our employee portal, powered by SQL databases, is designed to empower restaurant staff with seamless access to comprehensive information about our diverse range of spirits. It's a valuable resource for training, enhancing the guest experience, and boosting sales. Feel free to create an account to explore our tequila and mezcal databases!",
+    },
+    {
+      title: "SQL Employee Tracker",
+      link: "https://github.com/NickSweet1/SQL-Employee-Tracker",
+      video:
+        "https://drive.google.com/file/d/1eqmU_czspetV5ywkY922V9E9EqGC2Fod/view",
+      description:
+        "This project marks one of my early adventures into the world of SQL. The concept here is to create and manage an SQL database using CRUD operations (Create, Read, Update, and Delete). In this particular instance, I've crafted an employee database as an example. Feel free to explore further by clicking any of the links below to get a closer look!",
+    },
+    {
+      title: "Instabook API",
+      link: "https://github.com/NickSweet1/Instabook-API",
+      video:
+        "https://drive.google.com/file/d/1kVegxXUuWvxUN_vmwzz7hLfGGw--PTXT/view",
+      description:
+        "Welcome to my social media platform. It's like my little corner of the internet where users can connect, share posts, and chat. I've set up MongoDB to store user profiles, posts, and comments, and I've made it super easy to interact with the database using CRUD operations (Create, Read, Update, Delete). Although it's not a fully functioning site just yet, I've got the backend all set up and working seamlessly! If you'd like to see it in action, check out the video walkthrough—I'd love to show you around!",
+    },
+    {
+      title: "Custom ReadMe Generator",
+      link: "https://github.com/NickSweet1/Custom-Readme-Generator",
+      video:
+        "https://drive.google.com/file/d/1czSNL5Fw69sBzLzyxZEyvOw_dRGRyCpj/view",
+      description:
+        "This app was a game-changer during my bootcamp. It helps users create neat README files in Markdown format. You enter your details, and it does the rest, giving you a polished ReadMe.md file with a table of contents, badges, links, and more. It's like having a helpful documentation assistant!",
+    },
+    {
+      title: "Styled Password Generator",
+      link: "https://github.com/NickSweet1/Styled-Password-Generator",
+      sitelink: "https://nicksweet1.github.io/Styled-Password-Generator/",
+      description:
+        "This project holds a special place as one of my earliest creations. It's a password generator that relies on user input to craft a unique password comprising letters, numbers, and symbols. While my design skills have evolved considerably since then, it's a fun journey back to where it all started. Feel free to give it a try and see how it all began!",
+    },
+  ];
